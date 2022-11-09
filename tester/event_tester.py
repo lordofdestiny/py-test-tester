8e1e12bb9c6273e53f896e0bf58ce25675898c92
Branislav Djumic <djumicbranislav@outlook.com>
2022-11-09 21:41:40 +0100

    added handling for working directory creation

--- tester/event_tester.py
+++ tester/event_tester.py
@@ -4,12 +4,14 @@ from tester.language import Language
 from tester.program import Program
 from tester.logger import Logger
 from typing import List, Tuple
+import shutil
 import os
 
 
 class TestRunner:
-    def __init__(self, program: Program, logger=None) -> None:
+    def __init__(self, program: Program, working_dir, logger=None) -> None:
         self.program: Program = program
+        self.working_dir = working_dir
         self.logger: Logger = logger
         self.log: bool = logger is not None
 
@@ -17,21 +19,29 @@ class TestRunner:
         return [self.run(test) for test in tests]
 
     def run(self, test):
-        result = self.program.run(test)
+        result = self.program.run(test, self.working_dir)
 
         if self.log:
             self.logger.log(f"{test.name}")
             self.logger.log(test.get_report(result))
             self.logger.log(f"{'*' * 40}")
 
+        with os.scandir(self.working_dir) as entries:
+            for entry in entries:
+                if entry.is_dir() and not entry.is_symlink():
+                    shutil.rmtree(entry.path)
+                else:
+                    os.remove(entry.path)
+
         return result
 
 
 class Participant:
-    def __init__(self, source_file, executables_dir, logs_dir):
+    def __init__(self, working_dir, source_file, executables_dir, logs_dir):
+        self.working_dir = working_dir
         self.source_file: str = source_file
-        self.name: str
         self.id: str
+        self.name: str
         self.name, self.id = Participant.parse_name(source_file)
         self.executable_file: str = os.path.join(
             executables_dir,
@@ -93,6 +103,7 @@ class EventTester:
         self.executables_dir: str = os.path.join(root, settings["executables_dir"])
         self.tests_dir: str = os.path.join(root, settings["tests_dir"])
         self.logs_dir: str = os.path.join(root, settings["logs_dir"])
+        self.working_dir: str = os.path.join(root, "workdir")
 
         if not os.path.exists(self.tests_dir):
             raise FileNotFoundError(self.tests_dir)
@@ -103,7 +114,12 @@ class EventTester:
             raise FileNotFoundError(self.sources_dir)
 
         self.participants: List[Participant] = [
-            Participant(source_files, self.executables_dir, self.logs_dir)
+            Participant(
+                self.working_dir,
+                source_files,
+                self.executables_dir,
+                self.logs_dir,
+            )
             for source_files in self.__get_sources()
         ]
 
@@ -121,8 +137,18 @@ class EventTester:
         ]
 
     def test_all(self) -> None:
+        if not os.path.exists(self.working_dir):
+            os.makedirs(self.working_dir, exist_ok=True)
+        with os.scandir(self.working_dir) as entries:
+            for entry in entries:
+                if entry.is_dir() and not entry.is_symlink():
+                    shutil.rmtree(entry.path)
+                else:
+                    os.remove(entry.path)
         for participant in self.participants:
             self.test_participant(participant)
+
+        shutil.rmtree(self.working_dir)
 
     def test_participant(self, participant: Participant):
         try:
@@ -135,7 +161,7 @@ class EventTester:
             with Logger(participant.logs_file) as logger:
                 logger: Logger
 
-                runner: TestRunner = TestRunner(program, logger)
+                runner = TestRunner(program, participant.working_dir, logger)
                 participant.results: List[TestResult] = runner.run_all(self.tests)
                 successes: int = participant.get_successes()
                 points: float = participant.get_points()
